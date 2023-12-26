864a16ec6415f02c1140f6c160fc58d27fa751f9
SwipSwup <david.veigel2002@gmail.com>
2023-12-26 13:32:06 +0100

    Add file io

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+std::vector<std::string> getInput()
+{
+    std::vector<std::string> vector;
+
+    std::ifstream file("../puzzle_input.txt");
+    if (!file.is_open())
+    {
+        std::cout << "file doesn't exist" << std::endl;
+        return vector;
+    }
+
+    std::string line;
+    while (getline(file, line))
+    {
+        std::cout << line << std::endl;
+    }
+
+    file.close();
+    return vector;
+}
+
